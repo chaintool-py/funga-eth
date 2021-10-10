7f113d1cdc301738d658414e4234ee717bd1ca03
nolash <dev@holbrook.no>
2021-10-10 18:27:16 +0200

    Replace test keyfile

---
+++ run_tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+set -x
+default_pythonpath=$PYTHONPATH:.
+export PYTHONPATH=${default_pythonpath:-.}
+>&2 echo using pythonpath $PYTHONPATH
+for f in `ls tests/*.py`; do
+	python $f
+done
+set +x
+set +e
