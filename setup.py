a534f8ca1ece205c86491af4dcaae0dc3bfbc871
lash <dev@holbrook.no>
2022-05-04 18:09:01 +0000

    Avoid padding of missing nibble in address

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ f.close()
 
 setup(
         name="funga-eth",
-        version="0.6.0",
+        version="0.6.1",
         description="Ethereum implementation of the funga keystore and signer",
         author="Louis Holbrook",
         author_email="dev@holbrook.no",
