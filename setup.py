8672b901baea4d15d8e4982a9e9775e060716911
lash <dev@holbrook.no>
2022-11-14 07:51:55 +0000

    Change license to AGPL3, waive copyright

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ f.close()
 
 setup(
         name="funga-eth",
-        version="0.6.2",
+        version="0.6.3",
         description="Ethereum implementation of the funga keystore and signer",
         author="Louis Holbrook",
         author_email="dev@holbrook.no",
@@ -58,6 +58,6 @@ setup(
                 'eth-sign-msg=funga.eth.runnable.msg:main',
                 ],
             },
-        url='https://git.grassecon.net/chaintool/funga-eth',
+        url='https://git.defalsify.org/funga-eth.git',
         include_package_data=True,
         )
