240cbbf372da62257abbf65ddb115ef229e6695c
lash <dev@holbrook.no>
2023-03-29 10:29:41 +0100

    Add typed data signing support (ERC191 v0x01 / ERC712)

--- funga/eth/signer/defaultsigner.py
+++ funga/eth/signer/defaultsigner.py
@@ -11,6 +11,7 @@ from funga.signer import Signer
 from funga.eth.encoding import chain_id_to_v
 from funga.eth.message import to_personal_message
 from funga.eth.message import to_validator_message
+from funga.eth.message import to_typed_message
 
 logg = logging.getLogger(__name__)
 
@@ -62,24 +63,17 @@ class EIP155Signer(Signer):
 
 
     def sign_validator_message(self, address, validator, message, password=None):
-        #k = keys.PrivateKey(self.keyGetter.get(address, password))
-        #z = keys.ecdsa_sign(message_hash=g, private_key=k)
-        if type(message).__name__ == 'str':
-            logg.debug('signing message in "str" format: {}'.format(message))
-            #z = k.sign_msg(bytes.fromhex(message))
-            message = bytes.fromhex(message)
-        elif type(message).__name__ == 'bytes':
-            logg.debug('signing message in "bytes" format: {}'.format(message.hex()))
-            #z = k.sign_msg(message)
-        else:
-            logg.debug('unhandled format {}'.format(type(message).__name__))
-            raise ValueError('message must be type str or bytes, received {}'.format(type(message).__name__))
-
         message_to_sign = to_validator_message(message, validator, digest=True)
         z = self.sign_pure(address, message_to_sign, password)
         return z
 
-    
+   
+    def sign_typed_message(self, address, domain, message, password=None):
+        message_to_sign = to_typed_message(message, domain, digest=True)
+        z = self.sign_pure(address, message_to_sign, password)
+        return z
+
+
     # TODO: generic sign should be moved to non-eth context
     def sign_pure(self, address, message, password=None):
         pk = coincurve.PrivateKey(secret=self.keyGetter.get(address, password))
